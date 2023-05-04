29ae87f441f28a2cd55f34eb3678b1ec5e3d09c4
jaroslawjanas <jaroslawjanas.dev@gmail.com>
2023-05-04 22:42:12 +0200

    Refactoring of base code template

--- src/args.py
+++ src/args.py
@@ -10,6 +10,6 @@ parser.add_argument("--driver_path", help="Manualy specify Chromium driver path 
 parser.add_argument("--interval", help="How many hours between each voting loop (deafult: 5)", type=int, default=5)
 parser.add_argument("--width", help="Browser window width (default: 960)", type=int, default=960)
 parser.add_argument("--height", help="Browser window height (default: 960)", type=int, default=960)
-
+parser.add_argument("--incognito", help="Runs the browser in incognito mode (default: false)", action="store_true", default=False)
 
 args = parser.parse_args()
